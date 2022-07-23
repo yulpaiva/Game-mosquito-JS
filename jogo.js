0b71d69f15fd6f90041207cf3ca462501c11c123
unknown <yulbrynner73@gmail.com>
2022-07-23 02:43:39 -0300

    first commit

---
+++ jogo.js
@@ -0,0 +1,118 @@
+var height = 0
+var width = 0
+var vidas = 1
+var tempo = 10
+
+var criaMoscaTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?','')
+
+if(nivel === 'normal') {
+	//1500
+
+} else if (nivel === 'dificil'){
+	//1000
+	criaMoscaTempo = 1000
+} else if (nivel === 'chucknorris'){
+	//750
+	criaMoscaTempo = 750
+}
+
+
+
+function ajustaTamanhoPalcoJogo() {
+	height = window.innerHeight
+	width = window.innerWidth
+
+	console.log(width, height)
+}
+	
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if (tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+		window.location.href = 'vitoria.html'
+	} else {
+	document.getElementById('cronometro').innerHTML = tempo
+	}
+
+},1000)
+
+function posicaoRandomica(){
+
+	//remover a mosca anterior (caso exista)
+	if (document.getElementById('mosca')) {
+		document.getElementById('mosca').remove()
+
+		if(vidas > 3){
+			
+			window.location.href = 'fim_de_jogo.html'
+
+		} else {
+		document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png' 
+
+		vidas++
+		}
+	}
+
+	var posicaoX = Math.floor(Math.random() * width) - 90
+	var posicaoY = Math.floor(Math.random() * height) - 90
+
+	//operador ternario
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+
+	console.log(posicaoX, posicaoY)
+
+	//Criar o elemento html
+	var mosca = document.createElement('img')
+	mosca.src = 'imagens/mosca.png'
+	mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+	mosca.style.left = posicaoX + 'px'
+	mosca.style.top = posicaoY + 'px'
+	mosca.style.position = 'absolute'
+	mosca.id = 'mosca'
+	mosca.onclick = function () {
+		this.remove()
+	}
+
+	document.body.appendChild(mosca)
+
+	
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosca1'
+
+		case 1:
+			return 'mosca2'
+
+		case 2:
+			return 'mosca3'
+
+	}
+}
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+
+		case 1:
+			return 'ladoB'
+
+	}
+}
